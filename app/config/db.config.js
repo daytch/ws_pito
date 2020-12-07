f4d6f3975570a4742ba9f1216bf45f4444f752b9
njicker <njicker@gmail.com>
2020-12-07 16:18:13 +0700

    commit on master

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 var con = {
-  // host: "5.189.134.84",
-  host: "13.228.95.192",
-  // user: "root",
-  user: "remote",
-  // password: "Insy4AllahB3rk4H!!!",
-  password: "fauzi2006",
+  host: "5.189.134.84",
+  // host: "13.228.95.192",
+  user: "root",
+  // user: "remote",
+  password: "Insy4AllahB3rk4H!!!",
+  // password: "fauzi2006",
   database: "pito_db"
 };
 
